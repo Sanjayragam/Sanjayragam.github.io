3388a77d19ad19d143f72c6d64f7740bfb2f12e6
Sanjayragam <56439173+Sanjayragam@users.noreply.github.com>
2021-08-27 22:26:31 +0530

    UP

---
+++ script.js
@@ -0,0 +1,28 @@
+$(window).on('load', function(){
+  setTimeout(removeLoader, 2000); 
+});
+function removeLoader(){
+   $( "#loadingDiv" ).fadeOut(500, function() {
+   $( "#loadingDiv" ).remove(); 
+  });  
+}
+
+$(document).ready(function(){
+  var mouseX, mouseY;
+  var ww = $( window ).width();
+  var wh = $( window ).height();
+  var traX, traY;
+  
+  $(document).mousemove(function(e){
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    traX = ((4 * mouseX) / 570) + 40;
+    traY = ((4 * mouseY) / 570) + 50;
+    $(".title").css({"background-position": traX + "%" + traY + "%"});
+  });
+  
+  $("#menu").click(function(e){
+    $(this).toggleClass("active")
+    $("#header").toggleClass("active")
+  })
+});
