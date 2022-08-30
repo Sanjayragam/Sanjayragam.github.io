ebdfdcef8935339efbe54672555124854f50040e
Sanjayragam <56439173+Sanjayragam@users.noreply.github.com>
2022-08-30 17:06:43 +0530

    up

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-/*inspiration
-https://cz.pinterest.com/pin/830703093780869703/ */
+
+$(window).on('load', function(){
+  setTimeout(removeLoader, 3000); 
+});
+function removeLoader(){
+   $( "#loadingDiv" ).fadeOut(500, function() {
+   $( "#loadingDiv" ).remove(); 
+  });  
+}
+
+$(document).ready(function(){
+  var mouseX, mouseY;
+  var ww = $( window ).width();
+  var wh = $( window ).height();
+  var traX, traY;
+  
+  $(document).mousemove(function(e){
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    traX = ((4 * mouseX) / 570) + 40;
+    traY = ((4 * mouseY) / 570) + 50;
+    $(".title").css({"background-position": traX + "%" + traY + "%"});
+  });
+  
+  $("#menu").click(function(e){
+    $(this).toggleClass("active")
+    $("#header").toggleClass("active")
+  })
+});
+
+
+
+
+
